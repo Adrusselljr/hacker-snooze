6ef9a65fc63114630dd2ec4ac94b68fa7c52e598
Aaron Russell Jr <adrusselljr@Aarons-MBP.lan>
2021-11-23 15:13:47 -0500

    modified js/html/css

---
+++ main.js
@@ -0,0 +1,34 @@
+const main = $('main')
+
+const url = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+
+const makeHttpRequest = async () => {
+    const httpResponse = await fetch(url)
+    const topStoryIDs = await httpResponse.json()
+    return topStoryIDs
+}
+
+const storyRequest = async element => {
+    const storyUrl = `https://hacker-news.firebaseio.com/v0/item/${element}.json?print=pretty`
+    const httpResponse = await fetch(storyUrl)
+    const storyData = await httpResponse.json()
+    return storyData
+}
+
+const getTopStoryID = async () => {
+    const topStoryIDs = await makeHttpRequest()
+    topStoryIDs.forEach(async element => {
+        const storyData = await storyRequest(element)
+        const title = $(`
+            <div class="links">
+                <ul>
+                    <li><a href="${storyData.url}">${storyData.title}</a></li>
+                    <p>${storyData.score} points. By ${storyData.by}. ${storyData.descendants} comments</p>
+                </ul>
+            </div>
+        `)
+        main.append(title)
+        console.log(storyData)
+    })
+}
+getTopStoryID()
